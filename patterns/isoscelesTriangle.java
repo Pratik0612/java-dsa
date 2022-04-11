2d57ae2aef5a641cf4a6273f0e32527e8cde381e
Pratik0612 <pratikvmedewar@gmail.com>
2022-04-11 11:29:47 +0530

    stars updated in stars3

--- patterns/isoscelesTriangle.java
+++ patterns/isoscelesTriangle.java
@@ -2,27 +2,27 @@ package patterns;
 
 import java.util.Scanner;
 
-public class stars3 {
+public class isoscelesTriangle {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int i = 1;
         while(i<=n){
             int j = 1;
-            while (j<=n-i){
+            while(j<=n-i){
                 System.out.print(" ");
                 j++;
             }
             j = 1;
-            while (j<=i){
-                System.out.print("**");
+            while(j<=i){
+                System.out.print(j);
                 j++;
             }
-            j = 1;
-            while(j<i){
-                System.out.print("*");
-                j++;
+//            j-=2;
+            j = i-1;
+            while(j>=1){
+                System.out.print(j);
+                j--;
             }
             System.out.println();
             i++;
